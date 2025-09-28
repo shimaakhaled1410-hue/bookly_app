6e41a5696a3be11ec237e1c61135155e294ab55a
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-09-29 00:30:06 +0300

    refactor init state and navigate to home & organize folders in correct way

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/features/presentation/views/splash_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
