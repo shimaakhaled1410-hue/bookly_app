9383b1b6d31c0f8748714728ac69fe7dd290b385
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-09-29 19:58:25 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -14,21 +15,45 @@ class HomeViewBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
-          CustomAppBar(), 
+          CustomAppBar(),
           FeaturedBooksListView(),
-          SizedBox(
-            height: 50,
-          ),
-          Text(
-            'Best Seller',
-            style: Styles.titleMediam,
-          ),
-      
-        ]
+          SizedBox(height: 50),
+          Text('Best Seller', style: Styles.titleMediam),
+          BestSellerListViewItem(),
+        ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
 
-
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Column(children: [
+          
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
