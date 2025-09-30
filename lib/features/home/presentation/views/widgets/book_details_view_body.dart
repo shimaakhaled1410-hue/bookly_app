bbfb9e4783e2938be9c8d6e96d08b567337b4f07
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-09-30 22:01:39 +0300

    add rating to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 import 'custom_book_details_app_bar.dart';
 import 'custom_book_image.dart';
@@ -34,6 +35,8 @@ class BookDetailsViewBody extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -4,22 +4,35 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/styles.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
-        const Icon(FontAwesomeIcons.solidStar, color: Color(0xffFFDD4F)),
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          size: 14,
+          color: Color(0xffFFDD4F),
+        ),
         const SizedBox(width: 6.3),
         Text(
           '4.8',
           style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
         ),
         const SizedBox(width: 5),
-        Text(
-          '(245)',
-          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(245)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         ),
       ],
     );
