9db5b085d8be1d62d12eeee62058a908edbd1989
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-01 01:01:42 +0300

    create custom search text field

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -14,8 +16,10 @@ class CustomAppBar extends StatelessWidget {
           Image.asset(AssetsData.logo, height: 18),
           const Spacer(),
           IconButton(
-            onPressed: () {},
-            icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 24),
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
           ),
         ],
       ),
