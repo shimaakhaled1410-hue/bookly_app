f5ae2cee13ae5a23d14165426be4dcdc6f9efbe6
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-01 23:36:22 +0300

    implement fetch newest books local data source

--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -11,14 +11,14 @@ abstract class HomeLocalDataSource {
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-
-    
     var box = Hive.box<BookEntity>(kFeaturedBox);
     return box.values.toList();
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kNewestdBox);
+    return box.values.toList();
+    
   }
 }
