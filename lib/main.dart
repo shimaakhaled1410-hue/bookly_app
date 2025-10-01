80e173bce3d12c652a853730338b95b1f9bd8bdb
shimaakhaled1410-hue <shimaakhaled1410@gmail.com>
2025-10-01 23:24:40 +0300

    add Hive.initFlutter to main

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:bookly_app/constatnts.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   runApp(const BooklyApp());
 
-
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox(kFeaturedBox);
 }
